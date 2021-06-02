4149587cbf9003baa4dbab9b7ea61e43bf6f5fbf
Victor <livshits.victor@gmail.com>
2021-06-02 13:39:15 +0300

    Do not rename main pleeease

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Final {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         Invoker invoker = new Invoker();
